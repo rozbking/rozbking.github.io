0c06abf23a3e4022655757b23e60c4b7cf5579ee
rozbking <125500425+rozbking@users.noreply.github.com>
2024-06-21 14:49:53 -0700

    Create getImages.php

---
+++ getImages.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: application/json');
+
+$imagesDir = 'Website_photos_rbking/';
+$images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+$imageList = array();
+
+foreach($images as $image) {
+    $imageList[] = $image;
+}
+
+echo json_encode($imageList);
+?>
